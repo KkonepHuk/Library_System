8597f68441c51f019cfcdc41f1c598293cb680a9
Никитос <nikitos@MacBookAir.lan>
2025-05-15 00:19:01 +0300

    Основной блок кода перенесен в файл main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from library_system import LibrarySystem
+
+
 '''
 --1) Добавить чтение из csv файла--
 2) Улучшить хэш - функцию
 3) Сделать добавление книги в csv файл
 '''
+
+if __name__ == '__main__':
+    LS = LibrarySystem(10)
+    LS.from_csv('books.csv')
+    LS.show()
