b6415136c343f70675c9ca3ee36d017b3594e058
Никитос <nikitos@MacBook-Air-Tana.local>
2025-05-17 13:18:17 +0300

    Добавлена функция для проверки валидности ISBN, команды действий вынесены в отдельые функции

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from book import Book
 '''
 --1) Добавить чтение из csv файла--
 2) Улучшить хэш - функцию
-3) Сделать добавление книги в csv файл
-4) Сделать основной цикл программы с выбором действия
+--3) Сделать добавление книги в csv файл--
+--4) Сделать основной цикл программы с выбором действия--
+--5) Написать функцию для проверки валидности ISBN--
 '''
 
 def show_information():
@@ -22,40 +23,80 @@ def show_information():
 
 def do_action(command):
     if command == '1':
-        print('Список доступных книг:\n')
-        return LS.show()
+        get_list_of_books()
     elif command == '2':
-        isbn = input('Введите ISBN искомой книги: ') #нужно написать функцию для проверки валидности ISBN
-        book = LS.get(isbn)
-        if book == -1 or book == None:
-            print('Книги с данным ISBN не существует')
-        else:
-            print(book)
+        isbn = input('Введите ISBN искомой книги: ')
+        if is_isbn_valid(isbn):
+            find_book(isbn)
     elif command == '3':
-        isbn = input('Введите ISBN нужной книги: ') #нужно написать функцию для проверки валидности ISBN
-        book = LS.get(isbn)
-        if book == -1:
-            print('Книги с данным ISBN не существует')
-        else:
-            LS.change_status(isbn)
-            print('Статус книги успешно изменен!')
+        isbn = input('Введите ISBN нужной книги: ')
+        if is_isbn_valid(isbn):
+            change_book_status(isbn)
     elif command == '4':
-        isbn = input('Введите ISBN книги, которую хотите удалить: ') #нужно написать функцию для проверки валидности ISBN
-        book = LS.get(isbn)
-        if book == -1:
-            print('Книги с данным ISBN не существует')
-        else:
-            LS.remove(isbn)
-            print('Книга успешно удалена!')
+        isbn = input('Введите ISBN книги, которую хотите удалить: ')
+        if is_isbn_valid(isbn):
+            remove_book(isbn)
     elif command == '5':
+        add_book()
+    elif command == '6':
+        save_edits()
+
+def get_list_of_books():
+    print('Список доступных книг:\n')
+    return LS.show()
+
+def find_book(isbn):
+    book = LS.get(isbn)
+    if book == -1 or book == None:
+        print('Книги с данным ISBN не существует')
+    else:
+        print(book)
+
+def change_book_status(isbn):
+    book = LS.get(isbn)
+    if book == -1:
+        print('Книги с данным ISBN не существует')
+    else:
+        LS.change_status(isbn)
+        print('Статус книги успешно изменен!')
+
+def remove_book(isbn):
+    book = LS.get(isbn)
+    if book == -1:
+        print('Книги с данным ISBN не существует')
+    else:
+        LS.remove(isbn)
+        print('Книга успешно удалена!')
+
+def add_book():
+    isbn = input('Введите ISBN новой книги: ')
+    if is_isbn_valid(isbn):
         new_book = get_new_book()
         LS.add(new_book)
         print('Книга успешно добавлена!')
-    elif command == '6':
-        LS.save(CSV_FILE)
 
-def get_new_book():
-    isbn = input('Введите ISBN новой книги: ') #нужно написать функцию для проверки валидности ISBN
+def save_edits():
+    LS.save(CSV_FILE)
+    print('Все изменения успешно сохранены!')
+
+#Проверка корректности ISBN
+def is_isbn_valid(isbn):
+    isbn = isbn.replace('-', '').replace(' ', '')
+    check_digit = -100
+    result = False
+    if len(isbn) == 13 and isbn.isdigit():
+        honest_summ = sum([int(isbn[i]) for i in range(0, 12, 2)])
+        odd_summ = sum([int(isbn[i]) * 3 for i in range(1, 12, 2)])
+        summ = honest_summ + odd_summ
+        check_digit = (10 - summ % 10) % 10
+        result = check_digit == int(isbn[-1])
+    if result:
+        return True
+    print('Неверный ISBN')
+    return False
+
+
+def get_new_book(isbn):
     name = input('Введите название: ')
     autor = input('Введите автора: ')
     year = input('Введите год издания: ')
